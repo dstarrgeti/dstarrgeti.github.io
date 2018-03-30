b8821c53221d9b578899d1f2b1c2cb09ab7f4a0c
dstarrgeti <dstarr@getinnovated.com>
2018-03-30 19:01:46 -0400

    putting together front end dev test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$('.carousel').carousel({
-    interval: 2000
-})
+$("document").ready(function() {
+    $('.carousel').carousel({
+        interval: 2000
+    });
+
+    /* Sticky Nav for Navigation */
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 0) {
+            $('.navbar').removeClass('nav-top-pos');
+        }
+        if ($(window).scrollTop() >= 50) {
+            $('.navbar').addClass('nav-top-pos');
+
+        }
+    });
+});
+
+
