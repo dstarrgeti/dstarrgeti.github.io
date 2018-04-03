675f3f9fec6e5de3ce973d69d991dc734594e3c4
dstarrgeti <dstarr@getinnovated.com>
2018-04-03 09:27:06 -0400

    putting together front end dev test

--- js/main.js
+++ js/main.js
@@ -11,8 +11,17 @@ $("document").ready(function() {
         }
         if ($(window).scrollTop() >= 50) {
             $('.navbar').addClass('nav-top-pos');
-
         }
+        if ($(window).scrollTop() >= 50) {
+            $('#about').addClass('animated fadeInLeft');
+        }
+    });
+
+    $(document).on('click', '.navbar a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top-0
+        }, 1000);
     });
 });
 
